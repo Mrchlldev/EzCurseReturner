1939298e825db890c35d0b9077e0ff22c66522c4
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-21 20:58:42 +1000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -6,12 +6,52 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
 
-  public function onChat(){
-
-  }
-public function toxicreplies {
-
-
+  public function onChat(PlayerChatEvent $event) {
+        $player = $event->getPlayer();
+        $chat = $event->getMessage();
+if(Event->getMessage() === "fuck") {
+$this->getServer()->broadcastMessage("Oi STFU " . $player . " u Fat Pig, Gronk, Spanner, Flogwogger jackass <3");
 }
+    if(Event->getMessage() === "bitch") {
+$this->getServer()->broadcastMessage("yes, " . $player . " that is what you are<3");
+      
+}
+if(Event->getMessage() === "cunt") {
+$this->getServer()->broadcastMessage("yes a c*nt is delicious, shame you cant get any " . $player . " <3");
+}
+    if(Event->getMessage() === "nigger") {
+$this->getServer()->broadcastMessage("wow. such strong words from such a puny kid <3");
+}
+    if(Event->getMessage() === "nigga") {
+$this->getServer()->broadcastMessage("wow. such strong words from such a puny kid <3");
+}
+        if(Event->getMessage() === "asshole") {
+$this->getServer()->broadcastMessage("yes, an a-hole is where you sh- from. glad you pointed out something everyone has <3");
+}
+        if(Event->getMessage() === "ass") {
+$this->getServer()->broadcastMessage("yes, an ass is where you sh- from. glad you pointed out something everyone has <3");
+}
+     if(Event->getMessage() === "wanker") {
+$this->getServer()->broadcastMessage("i see you call someone a wanker, its a shame you cant wank with that tic tac of yours <3");
+}
+     if(Event->getMessage() === "fucktard") {
+$this->getServer()->broadcastMessage("are you talking about having intercourse with a retard, might aswell f- urself LOOOL <3");
+}
+     if(Event->getMessage() === "retard") {
+$this->getServer()->broadcastMessage("i love how much you like describing yourself <3");
+}
+     if(Event->getMessage() === "shit") {
+$this->getServer()->broadcastMessage("yes, shit is excrement from an animal <3");
+}
+     if(Event->getMessage() === "libtard") {
+$this->getServer()->broadcastMessage("are you righting a biography on your life? <3");
+}
+     if(Event->getMessage() === "fucknuckle") {
+$this->getServer()->broadcastMessage("you wanna fuck knuckles the hedgehog, well i guess you do you, might as well call you mr hands 2.0. <3");
+}
+     if(Event->getMessage() === "whore") {
+$this->getServer()->broadcastMessage("i believe that is an accurate description of your status <3");
+}
+  }
 
 }
