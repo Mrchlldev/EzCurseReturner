005f0a86f758b788092897b907c4a7a99d491305
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-21 21:01:10 +1000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -29,7 +29,7 @@ $this->getServer()->broadcastMessage("wow. such strong words from such a puny ki
 $this->getServer()->broadcastMessage("yes, an a-hole is where you sh- from. glad you pointed out something everyone has <3");
 }
         if(Event->getMessage() === "ass") {
-$this->getServer()->broadcastMessage("yes, an ass is where you sh- from. glad you pointed out something everyone has <3");
+$this->getServer()->broadcastMessage("yes, an a- is where you sh- from. glad you pointed out something everyone has <3");
 }
      if(Event->getMessage() === "wanker") {
 $this->getServer()->broadcastMessage("i see you call someone a wanker, its a shame you cant wank with that tic tac of yours <3");
@@ -41,13 +41,13 @@ $this->getServer()->broadcastMessage("are you talking about having intercourse w
 $this->getServer()->broadcastMessage("i love how much you like describing yourself <3");
 }
      if(Event->getMessage() === "shit") {
-$this->getServer()->broadcastMessage("yes, shit is excrement from an animal <3");
+$this->getServer()->broadcastMessage("yes, sh- is excrement from an animal <3");
 }
      if(Event->getMessage() === "libtard") {
 $this->getServer()->broadcastMessage("are you righting a biography on your life? <3");
 }
      if(Event->getMessage() === "fucknuckle") {
-$this->getServer()->broadcastMessage("you wanna fuck knuckles the hedgehog, well i guess you do you, might as well call you mr hands 2.0. <3");
+$this->getServer()->broadcastMessage("you wanna fu- knuckles the hedgehog, well i guess you do you, might as well call you mr hands 2.0. <3");
 }
      if(Event->getMessage() === "whore") {
 $this->getServer()->broadcastMessage("i believe that is an accurate description of your status <3");
