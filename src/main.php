de8a0194a82dc96b2b173091a93b890663f68b47
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-21 20:47:26 +1000

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EzMinecraftPlugins\EzCurseReturner;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+  public function onChat(){
+
+  }
+public function toxicreplies {
+
+
+}
+
+}
