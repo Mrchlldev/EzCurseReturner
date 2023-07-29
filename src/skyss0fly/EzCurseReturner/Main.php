139067bcb66b11d9db0373e522fc77817877443b
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2023-07-29 14:49:43 +0700

    Update Main.php

--- src/skyss0fly/EzCurseReturner/Main.php
+++ src/skyss0fly/EzCurseReturner/Main.php
@@ -13,11 +13,17 @@ class Main extends PluginBase implements Listener {
   if((strpos($chat, "stfu") !== false) {
        $this->getServer()->broadcastMessage("Calm down  " . $player . " there's no need for this behavior <3");
   }
-  if((strpos($chat, "shopmenu") !== false) {
-       $this->getServer()->broadcastMessage("==[ §l§bShop Menu§r ]==\n§l§b» §r§e/shop§r\n§l§b» §r§e/eshop§r\n§l§b» §r§e/market§r\n§l§b» §r§e/sell§r\n§l§b» §r§e/cs§r");
-     }
-  if((strpos($chat, "menu") !== false) {
-       $this->getServer()->broadcastMessage("Hello §e@" . $player . "§f!\n\n§l§b» §r§eshopmenu§r\n§l§b» §r§etpmenu§r") {
+  if((strpos($chat, "!rankmenu") !== false) {
+       $this->getServer()->broadcastMessage("==[ §l§bRank Menu§r ]==\n§l§b» §r§e/milk§r\n§l§b» §r§e/nick§r\n§l§b» §r§e/cw§r\n§l§b» §r§e/cskin§r\n§l§b» §r§e/vanish§r\n§l§b» §r§e/vs§r\n§l§b» §r§e/uvs§r\n§l§b» §r§e/pv 1,2,3,....10§r\n§l§b» §r§e/fly§r\n§l§b» §r§e/feed§r\n§l§b» §r§e/heal§r\n§l§b» §r§e/gmc§r\n§l§b» §r§e/gms§r\n§l§b» §r§e/size§r\n§l§b» §r§e/say§r\n§l§b» §r§elainnya cek di §b(/ranks)§e!!§r");
+  }
+  if((strpos($chat, "!shopmenu") !== false) {
+       $this->getServer()->broadcastMessage("==[ §l§bShop Menu§r ]==\n§l§b» §r§e/shop§r\n§l§b» §r§e/eshop§r\n§l§b» §r§e/market§r\n§l§b» §r§e/sell§r\n§l§b» §r§e/cs§r\n§l§b» §r§e/keyshop§r");
+  }
+  if((strpos($chat, "!tpmenu") !== false) {
+       $this->getServer()->broadcastMessage("==[ §l§bTeleport Menu§r ]==\n§l§b» §r§e/mine§r\n§l§b» §r§e/pvp§r\n§l§b» §r§e/survival§r\n§l§b» §r§e/nether§r\n§l§b» §r§e/theend§r\n§l§b» §r§e/warp§r\n§l§b» §r§e/tpa§r\n§l§b» §r§e/tpaaccept§r\n§l§b» §r§e/home§r\n§l§b» §r§e/tpadecline§r\n§l§b» §r§e/back§r\n§l§b» §r§e/pwarp§r");
+  }
+  if((strpos($chat, "!menu") !== false) {
+       $this->getServer()->broadcastMessage("Hello §e@" . $player . "§f!\n\n§l§b» §r§!eshopmenu§r\n§l§b» §r§e!tpmenu§r") {
   }
   if((strpos($chat, "fuck") !== false) {
        $this->getServer()->broadcastMessage("yes thats an act of intercourse " . $player . "<3");
